10d3dc43d0c04738fedaded35873f9de2eab652e
inception <tterribi@debian.tterribi@student.42roma.it>
2023-10-24 11:33:40 -0500

    updates at setup.sh

--- setup.sh
+++ setup.sh
@@ -1,7 +1,6 @@
-sudo apt update #2> dev/null
-sudo apt upgrade -y #2> dev/null
-sudo apt install -y git #2> dev/null
-sudo apt install -y make #2> dev/null
-sudo apt install -y vim #2> dev/null
-
-#sudo apt install vscode
+@sudo apt update 2> /dev/null
+@sudo apt upgrade -y 2> /dev/null
+@sudo apt install -y git 2> /dev/null
+@sudo apt install -y make 2> /dev/null
+@sudo apt install -y vim 2> /dev/null
+@sudo apt install -y vscode 2> /dev/null
