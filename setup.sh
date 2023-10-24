073fcf192e21ac871c420b1294d5967000802b78
inception <tterribi@debian.tterribi@student.42roma.it>
2023-10-24 11:13:59 -0500

    initial commit

---
+++ setup.sh
@@ -0,0 +1,7 @@
+sudo apt update #2> dev/null
+sudo apt upgrade -y #2> dev/null
+sudo apt install -y git #2> dev/null
+sudo apt install -y make #2> dev/null
+sudo apt install -y vim #2> dev/null
+
+#sudo apt install vscode
