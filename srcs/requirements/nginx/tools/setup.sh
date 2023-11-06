ce47909a58754972333ffcd00ccafa4859ac644b
bonobo <nanaki1307@gmail.com>
2023-11-06 12:47:00 -0500

    push

--- srcs/requirements/nginx/tools/setup.sh
+++ srcs/requirements/nginx/tools/setup.sh
@@ -2,4 +2,4 @@ echo "[Nginx] Creating SSL Certificate"
 openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout /etc/ssl/gfezzuog.42.fr.key -out /etc/ssl/gfezzuog.42.fr.crt -subj "/C=IT/L=Rome/O=42Roma/OU=student/CN=gfezzuog.42.fr"
 
 echo "[Nginx] starting Nginx"
-nginx -g " deamon off"
+nginx -g " daemon off;"
